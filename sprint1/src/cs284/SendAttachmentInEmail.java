9ef9bc88beb0fd8891fba8fa6cf640e876eb1e0d
Fook <Fook@DESKTOP-3VHUGQI>
2017-11-27 15:40:24 +0700

    class email

---
+++ sprint1/src/cs284/SendAttachmentInEmail.java
@@ -0,0 +1,80 @@
+package cs284;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class SendAttachmentInEmail {
+   public static void main(String[] args) {
+      // Recipient's email ID needs to be mentioned.
+      String to = "fphutha@gmail.com";
+
+      // Sender's email ID needs to be mentioned
+      String from = "cstusendgrade@gmail.com";
+
+      final String username = "cstusendgrade";
+      final String password = "ilovecs284";
+
+      String host = "smtp.gmail.com";
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.port", "25");
+
+      Session session = Session.getInstance(props,
+         new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+               return new PasswordAuthentication(username, password);
+            }
+         });
+
+      try {
+         Message message = new MimeMessage(session);
+
+         message.setFrom(new InternetAddress(from));
+
+         message.setRecipients(Message.RecipientType.TO,
+            InternetAddress.parse(to));
+
+         message.setSubject("CS284");
+
+         BodyPart messageBodyPart = new MimeBodyPart();
+
+         messageBodyPart.setText("Grade CS284");
+
+         Multipart multipart = new MimeMultipart();
+
+         multipart.addBodyPart(messageBodyPart);
+
+         messageBodyPart = new MimeBodyPart();
+         String filename = "grade.xls";
+         DataSource source = new FileDataSource(filename);
+         messageBodyPart.setDataHandler(new DataHandler(source));
+         messageBodyPart.setFileName(filename);
+         multipart.addBodyPart(messageBodyPart);
+
+         message.setContent(multipart);
+
+         Transport.send(message);
+
+         System.out.println("Sent message successfully....");
+  
+      } catch (MessagingException e) {
+         throw new RuntimeException(e);
+      }
+   }
+}
